bb9022fcc35ffbf1a6f7ff9ddf6a78242d3bc778
Yukiko Matsumoto(Hori) <one_yukiko@hotmail.com>
2014-09-23 17:18:16 +0900

    gulp-connect, gulp-webserverを試す

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
   csslint = require('gulp-csslint'),
   cssmin = require('gulp-cssmin'),
   rename = require('gulp-rename'),
+  //connect = require('gulp-connect'),
+  webserver = require('gulp-webserver'),
   watch = require('gulp-watch'), //不要？
   nib = require('nib');
 
@@ -37,17 +39,53 @@ gulp.task('csslint', function() {
     .pipe(gulp.dest(DEST));
 });*/
 
+// https://www.npmjs.org/package/gulp-connect
+/*gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    port: 8080,
+    livereload: true
+  });
+});
+gulp.task('html', function() {
+  gulp.src(DEST)
+    .pipe(connect.reload());
+});
+*/
+
+// https://github.com/schickling/gulp-webserver
+gulp.task('webserver', function() {
+  gulp.src(DEST)
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true,
+      port: 9006
+    }));
+});
+
+// https://github.com/floatdrop/gulp-watch/blob/master/docs/readme.md
 gulp.task('watch', function() {
+  /*gulp.watch(DEST), ['html'];
+  gulp.watch(SRC), ['stylus', 'csslint'];*/
+
+/*
+  gulp.watch(SRC, function(files, cb) {
+    gulp.start(['stylus', 'csslint'], cb);
+  });*/
+
+
   gulp.src(SRC)
-/*  .pipe(watch(SRC, function(files) {
+  .pipe(watch(SRC, function(files, cb) {
     return files.pipe(gulp.dest(DEST));
-  }))*/
+  }))
   .pipe(gulp.dest(DEST));
 });
 
 
 // task to run
-gulp.task('default', ['stylus', 'csslint', 'watch']);
+//gulp.task('default', ['stylus', 'csslint', 'watch']);
+gulp.task('default', ['stylus', 'csslint', 'webserver', 'watch']);
 //gulp.task('default', ['stylus', 'csslint', 'cssmin', 'watch']);
 
 
