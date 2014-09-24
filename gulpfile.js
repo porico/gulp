46c472b581ac1a841d44e169c6e3a8f9cc34ba4a
porico <porico@imjp.co.jp>
2014-09-24 18:14:42 +0900

    gulp-browserifyを試す
    gulp-browserifyはblacklisted？

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var gulp = require('gulp'),
   csslint = require('gulp-csslint'),
   cssmin = require('gulp-cssmin'),
   rename = require('gulp-rename'),
+  browserify = require('gulp-browserify'),
+  source = require('vinyl-source-stream'), //gulp-browserifyがblacklist化したためこれを併用する方法を推奨
   //connect = require('gulp-connect'),
   webserver = require('gulp-webserver'), //gulp-connectの後継
   watch = require('gulp-watch'), //不要？
@@ -18,6 +20,7 @@ var DEST = './htdocs/';
 var DEST_CSS = './htdocs/**/*.css';
 var SRC = './src/**/*.styl';
 var SRC_IMG = './src/**/*.{gif,jpg,png,svg}';
+var SRC_JS = './src/**/.js';
 
 
 // task config
@@ -38,6 +41,31 @@ gulp.task('csslint', function() {
     .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
     .pipe(csslint())
     .pipe(csslint.reporter());
+});
+
+gulp.task('browserify', function() {
+/*
+  // blacklist化したので以下のように記述せよ、とあるが、bundle()でエラーになる
+  // http://qiita.com/mizchi/items/10a8e2b3e6c2c3235e61
+  return browserify(SRC_JS)
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .bundle()
+    // Pass desired output filename to vinyl-source-stream
+    .pipe(source('bundle.js'))
+    // Start piping stream to tasks!
+    .pipe(gulp.dest(DEST));
+*/
+  //
+  gulp.src(SRC_JS, { read: false })
+    .pipe(browserify({
+      transform: ['coffeeify'],
+      extensions: ['coffee']
+      // insertGlobals : true,
+      // debug : !gulp.env.production
+    }))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest(DEST));
+
 });
 
 gulp.task('imagemin', function() {
@@ -114,8 +142,8 @@ gulp.task('watch', function() {
 
 // task to run
 //gulp.task('default', ['stylus', 'csslint', 'watch']);
-gulp.task('default', ['stylus', 'csslint', 'webserver', 'watch']);
+gulp.task('default', ['stylus', 'csslint', 'browserify', 'webserver', 'watch']);
 //gulp.task('default', ['stylus', 'csslint', 'cssmin', 'watch']);
 
-gulp.task('build', ['imagemin', 'stylus', 'csslint']);
+gulp.task('build', ['imagemin', 'stylus', 'csslint', 'browserify']);
 
